343d0ed0886f35bfdd8bb99d188a7dc34e836cce
65010902_Racha_Tungtrakool <65010902@kmitl.ac.th>
2025-04-07 02:31:47 +0700

    can work well , can show UI  not debug lcd

--- MAIN.vhdl
+++ MAIN.vhdl
@@ -11,6 +11,11 @@ port
     byte_press : in std_logic_vector(7 downto 0);
     clk_read_byte : in std_logic;
 
+
+
+    dir_motor_out : out std_logic;
+    step_motor_out : out std_logic;
+
     lcd_rw : out std_logic;
     lcd_rs : out std_logic;
     lcd_e : out std_logic;
@@ -35,6 +40,18 @@ port
 );
 end component;
 
+component InterfaceStepperMotor
+port
+(
+    clk : in std_logic;         -- 20MHz
+    en : in std_logic;
+    dir_in : in std_logic;
+
+    dir_out: out std_logic;
+    step: out std_logic
+);
+end component;
+
 
 signal signal_byte_send : std_logic_vector(127 downto 0);
 signal count_clk : integer range 0 to 40000000 := 0;
@@ -56,7 +73,16 @@ signal sentence_display : std_logic_vector(127 downto 0);
 
 
 signal count_2sec : integer range 0 to 40000000 := 0;
-constant ORIGINAL_CLK_HZ : integer := 20000000
+constant ORIGINAL_CLK_HZ : integer := 20000000;
+
+
+
+-----------------control stepper motor--------------------
+signal signal_en_motor : std_logic := '0';
+signal signal_dir_motor : std_logic := '0';
+
+
+signal signal_is_door_open : std_logic := '0';
 
 
 
@@ -76,6 +102,17 @@ begin
         lcd_rs => lcd_rs,
         lcd_e  => lcd_e,
         data_out => data_out
+    );
+
+    place_InterfaceStepperMotor : InterfaceStepperMotor
+    port map
+    (
+        clk => clk,
+        en => signal_en_motor,
+        dir_in => signal_dir_motor,
+    
+        dir_out => dir_motor_out,
+        step => step_motor_out
     );
 
 
@@ -150,7 +187,7 @@ begin
     end process;
 
 
-    process(signal_current_byte_press)
+    process(signal_current_byte_press, signal_main_state)
     begin 
             case (signal_main_state) is
 
@@ -217,17 +254,24 @@ begin
 
                 when 8 =>
                     if(count_2sec = 40000000) then 
-                        signal_main_next_state <= 0;
+                        signal_main_next_state <= 10;
                     else 
                         signal_main_next_state <= 8;
                     end if;
 
                 when 9 =>
                     if(count_2sec = 40000000) then 
-                        signal_main_next_state <= 0;
+                        signal_main_next_state <= 10;
                     else 
                         signal_main_next_state <= 9;
                     end if; 
+
+                when 10 =>
+                    if signal_is_door_open = '0' then 
+                        signal_main_next_state <= 0;
+                    else
+                        signal_main_next_state <= 10;
+                    end if;
 
                 when others =>
                     signal_main_next_state <= 0;
@@ -285,6 +329,14 @@ begin
         if rising_edge(clk) then
 
             case(signal_main_state) is
+                when 0 | 1 =>
+                    display_debug <= '0';
+                when 2 | 3 =>
+                    display_debug <= '0';
+                when 4 =>
+                    display_debug <= '0';
+                when 5 | 6 | 7 =>
+                    display_debug <= '0';
                 when 8 | 9 => 
                     display_debug <= '0';
                 when others =>
@@ -294,10 +346,48 @@ begin
     end process;
 
     process(clk)        -- process sentence_display
+
+    -- Initial ...
+    -- X"49" & X"6E" & X"69" & X"74" & X"69" & X"61" & X"6C" & X"20" & X"2E" & X"2E" & X"2E" & X"20" & X"20" & X"20" & X"20" & X"20"
+    -- Enter Password  
+    -- X"45" & X"6E" & X"74" & X"65" & X"72" & X"20" & X"50" & X"61" & X"73" & X"73" & X"77" & X"6F" & X"72" & X"64" & X"20" & X"20"
+
+
     begin
         if rising_edge(clk) then
 
             case(signal_main_state) is
+                when 0 =>
+                    sentence_display <= X"49" & X"6E" & X"69" & X"74" & X"69" 
+                                        & X"61" & X"6C" & X"20" & X"2E" & X"2E" 
+                                        & X"2E" & X"20" & X"20" & X"20" & X"20" 
+                                        & X"20";
+                when 1 =>
+                    sentence_display <= X"45" & X"6E" & X"74" & X"65" & X"72" 
+                                        & X"20" & X"50" & X"61" & X"73" & X"73" 
+                                        & X"77" & X"6F" & X"72" & X"64" & X"20" 
+                                        & X"20";
+                when 2 =>
+                    sentence_display <= press_fir_digit & X"20" & X"20" & X"20" & X"20" 
+                                        & X"20" & X"20" & X"20" & X"20" & X"20"
+                                        & X"20" & X"20" & X"20" & X"20" & X"20"
+                                        & X"20";
+                when 3 =>
+                    sentence_display <= press_fir_digit & X"20" & X"20" & X"20" & X"20" 
+                                        & X"20" & X"20" & X"20" & X"20" & X"20"
+                                        & X"20" & X"20" & X"20" & X"20" & X"20"
+                                        & X"20";
+                when 4 =>
+                    sentence_display <= press_fir_digit & press_sec_digit & X"20" & X"20" & X"20" 
+                                        & X"20" & X"20" & X"20" & X"20" & X"20"
+                                        & X"20" & X"20" & X"20" & X"20" & X"20"
+                                        & X"20";
+                when 5 | 6 | 7 =>
+                    sentence_display <= press_fir_digit & press_sec_digit & X"20" & X"20" & X"20" 
+                                        & X"20" & X"20" & X"20" & X"20" & X"20"
+                                        & X"20" & X"20" & X"20" & X"20" & X"20"
+                                        & X"20";
+                
                 when 8 => 
                     sentence_display <= X"43" & X"4F" & X"52" & X"20" & X"20"
                                         & X"20" & X"20" & X"20" & X"20" & X"20"
@@ -319,7 +409,7 @@ begin
     end process;
 
 
-    process(clk)
+    process(clk)                -- process for count_2sec
     begin
         if rising_edge(clk) then
             case(signal_main_state) is
@@ -331,6 +421,50 @@ begin
                     end if;
                 when others =>
                     count_2sec <= 0;
+            end case;
+        end if;
+    end process;
+
+
+    process(clk)
+        variable count_open_50_clk : integer range 0 to 4000000 := 0;            -- make stepper motor rotate 90 deg
+        variable count_close_50_clk : integer range 0 to 4000000 := 0;            -- make stepper motor rotate 90 deg
+
+    begin
+        if rising_edge(clk) then
+            case(signal_main_state) is
+                when 8 | 9 =>
+                    if count_open_50_clk < 4000000 then
+                        signal_dir_motor <= '1';
+                        signal_en_motor <= '1';
+                        count_open_50_clk := count_open_50_clk + 1;
+                        signal_is_door_open <= '1';   -- DOOR  OPEN !!!
+                    else
+                        signal_dir_motor <= '0';
+                        signal_en_motor <= '0';
+                        count_open_50_clk := count_open_50_clk;
+                        signal_is_door_open <= '1';     -- DOOR  OPEN !!!
+                    end if;
+                
+                when 10 =>
+                    if count_close_50_clk < 4000000 then
+                        signal_dir_motor <= '0';
+                        signal_en_motor <= '1';
+                        count_close_50_clk := count_close_50_clk + 1;
+                        signal_is_door_open <= '1';         -- DOOR STILL OPEN !!!
+                    else
+                        signal_dir_motor <= '0';
+                        signal_en_motor <= '0';
+                        count_close_50_clk := count_close_50_clk;
+                        signal_is_door_open <= '0';         -- DOOR CLOSE!!
+                    end if;
+                when others =>
+                    signal_dir_motor <= '0';
+                    signal_en_motor <= '0';
+                    count_open_50_clk := 0;
+                    count_close_50_clk := 0;
+                    signal_is_door_open <= '0';         -- DOOR CLOSE
+
             end case;
         end if;
     end process;
